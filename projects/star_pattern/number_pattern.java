4a27cc95a906433af012a936b13bf22a6810539f
unknown <amitraikwar993613@gmail.com>
2026-02-12 19:26:34 +0530

    notes

--- projects/star_pattern/number_pattern.java
+++ projects/star_pattern/number_pattern.java
@@ -1,37 +1,35 @@
 
 import java.util.Scanner;
 
-public class script {
+public class number_pattern {
 
     public static void main() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int num = 1;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n-1; i++) {
             for (int j = n - i; j >= 1; j--) {
                 System.out.print(" ");
             }
-            // for(int i=n ; i>=1 ; i--){
+
             for (int j = 1; j <= i; j++) {
-                // for(int j=i ;j>=1; j--){
+               
                 System.out.print(num + " ");
-                // System.out.print(i);
-                // System.out.print();
+                
                 num++;
             }
             System.out.println();
         }
         int num1 = 1;
-        for (int i = n-1; i >= 1; i--) {
+        for (int i = n-2; i >= 1; i--) {
             for (int j = n - i; j >= 1; j--) {
                 System.out.print(" ");
             }
-            // for(int i=n ; i>=1 ; i--){
+           
             for (int j = i; j >= 1; j--) {
-                // for(int j=i ;j>=1; j--){
+               
                 System.out.print(num1 + " ");
-                // System.out.print(i);
-                // System.out.print();
+                
                 num1++;
             }
             System.out.println();

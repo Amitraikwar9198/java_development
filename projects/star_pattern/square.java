3cafb62d918d1264fdb24c8e48e15ce83eae279c
unknown <amitraikwar993613@gmail.com>
2026-02-23 22:59:21 +0530

    notes

--- projects/star_pattern/square.java
+++ projects/star_pattern/square.java
@@ -4,17 +4,8 @@ public class square{
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the Number : ");
         int n = sc.nextInt();
-        for(int i=1 ;i<=n ;i++){
-            for(int j=1; j<=n; j++){
-                if(i==1|| i==n || j==1 || j==n){
-                    System.out.print("* ");
-                }else{
-                    System.out.print("  ");
-                }
+        
 
-            }
-            System.out.println();
-        }
 
 
     }

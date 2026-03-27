5b2fcd486179591208ec6d726378f11fd50624f3
unknown <amitraikwar993613@gmail.com>
2026-03-27 23:34:59 +0530

    code

---
+++ projects/practice/even_odd_number.java
@@ -0,0 +1,15 @@
+public class even_odd_number{
+    public static void main(){
+        int n=5;
+    if (n % 2 == 0)
+    {
+        System.out.println("even");
+    }
+    else
+    {
+        System.out.println("odd");
+    }
+
+    }
+
+}
